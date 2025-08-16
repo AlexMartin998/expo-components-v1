6a059741a5d550001a6454d7c5cab29ff0bce39b
Adrian <alextravc.342@gmail.com>
2025-08-16 11:40:09 -0500

    feat: ThemedView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { Text } from 'react-native';
 import './../global.css';
 
 export default function RootLayout() {
+  // dark mode ----
+  const backgroundColor = useThemeColor({}, 'background');
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -19,12 +28,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView
+      style={{ backgroundColor: backgroundColor, flex: 1 }} // upd bg color with useThemeColor
+    >
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemedView useMargin>
+          <Text className="mt-10 text-3xl text-light-text dark:text-dark-text">
+            Hello
+          </Text>
+        </ThemedView>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
---
+++ presentation/shared/ThemedView.tsx
@@ -0,0 +1,49 @@
+import { useThemeColor } from '@/hooks/useThemeColor';
+import React from 'react';
+import { View, ViewProps } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+interface ThemedViewProps extends ViewProps {
+  className?: string;
+  useMargin?: boolean;
+  useSafeArea?: boolean;
+
+  bgColor?: string;
+}
+
+const ThemedView = ({
+  style,
+  className,
+  useMargin = false,
+  useSafeArea = false,
+  bgColor,
+  children,
+  ...props
+}: ThemedViewProps) => {
+  // bg color ----
+  const themeBackground = useThemeColor({}, 'background');
+  const backgroundColor = bgColor ?? themeBackground;
+
+  // safe area ----
+  const safeArea = useSafeAreaInsets();
+
+  return (
+    <View
+      // className="bg-light-background dark:bg-dark-background"
+      style={[
+        {
+          backgroundColor: backgroundColor,
+          flex: 1,
+          paddingTop: useSafeArea ? safeArea.top : 0,
+          marginHorizontal: useMargin ? 10 : 0,
+        },
+      ]}
+      className={className}
+      {...props}
+    >
+      {children}
+    </View>
+  );
+};
+
+export default ThemedView;
