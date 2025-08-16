dfeb0023ac3f1bf881733fa3feb37c8a281f16c9
Adrian <alextravc.342@gmail.com>
2025-08-16 17:15:26 -0500

    feat: ThemedCard

--- app/switches/index.tsx
+++ app/switches/index.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+
+import ThemedCard from '@/presentation/shared/ThemedCard';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { Switch } from 'react-native';
 
 const Switches = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
-    <View>
-      <Text>Switches</Text>
-    </View>
+    <ThemedView useMargin>
+      <ThemedCard>
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default Switches;
